0e64da51de368d489503bb6c8044f44dea9fc567
rajayaseen <yaseenraja340@gmail.com>
2023-08-08 03:59:14 -0700

    IF else with math.random and round

---
+++ nif.js
@@ -0,0 +1,15 @@
+var x = Math.random() * 10; //0-1 (0.1,0.2 upto 1)
+if (x > 8) {
+    console.log("value must be moe thsn ".concat(x));
+}
+else if (x < 5) {
+    console.log("value must be less thsn ".concat(x));
+}
+else {
+    console.log("hhh");
+}
+// now example of round
+// let y=Math.round(Math.random()*20);
+// if (y > 5){
+//     console.log(y)
+// }
---
+++ nif.ts
@@ -0,0 +1,10 @@
+let x=Math.random()*10; //0-1 (0.1,0.2 upto 1)
+if (x > 8){
+    console.log(`value must be moe thsn ${x}`)
+} else if (x < 5){
+    console.log(`value must be less thsn ${x}`)
+} else {
+    console.log("hhh")
+}
+
+
