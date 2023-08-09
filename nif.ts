aed0d1f69f4ada29cd4db5b739828ae05c3e5f0d
rajayaseen <yaseenraja340@gmail.com>
2023-08-09 02:04:33 -0700

    Array with example

--- nif.ts
+++ nif.ts
@@ -9,26 +9,41 @@
 
 
 
-let country= ["Pakistan", "Turkey", "China"]
-let age= Math.round(Math.random()*90)
-let domocile= ["sindh", "punjab", "balochistan", "khyber"]
-// let index=Math.random()*10;
-// Domicile= sindh
+// let country= ["Pakistan", "Turkey", "China"]
+// let age= Math.round(Math.random()*90)
+// let domocile= ["sindh", "punjab", "balochistan", "khyber"]
+// // let index=Math.random()*10;
+// // Domicile= sindh
 
-if (country[0] === "Pakistan"){
-    if (age >= 18) {
-        console.log("Here is your ticket")
-    }
-else{
-    console.error("Age Restriction")
-}
- } else if(country[1] == "Turkey")
-  if (age>=30){
+// if (country[0] === "Pakistan"){
+//     if (age >= 18) {
+//         console.log("Here is your ticket")
+//     }
+// else{
+//     console.error("Age Restriction")
+// }
+//  } else if(country[1] == "Turkey")
+//   if (age>=30){
 
-  }else{
+//   }else{
 
-  }
-  //.....
-  else {
-    console.log("Invalid country")
-}
+//   }
+//   //.....
+//   else {
+//     console.log("Invalid country")
+// }
+
+let Team =["waheed","yaseen","sultan","arsal"]
+console.log(`Hum sub ka math ha or Raja ${Team[0]} saab captain hayn`)
+console.log(`team ma aik or banda add karna par gya ha so ma Huzaifa ko b add kar raha ma socha ${Team}ko bata du`)
+Team.push("huzaifa")// yh akhit ma add kary ga
+Team.pop() //yeh wala akhri remove kry ga 
+console.log(Team)
+Team.shift() ///yeh wala pehla remove kry ga
+console.log(Team)
+Team.unshift("usama") // yh pehla add kary ga 
+console.log(Team)
+Team.splice(2,0,"Waheed","haseeb")// kahi par b addition karni ho
+console.log(Team)
+let TeamA = Team.slice(0,4)// starting number sy agy or endign number sy pehly pehly tk new array ma
+console.log(TeamA)
