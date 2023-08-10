f9d71e759d23595170a7e5f31da2fa8f6efe3c7d
rajayaseen <yaseenraja340@gmail.com>
2023-08-10 02:48:59 -0700

    while or do loop

---
+++ wloop.js
@@ -0,0 +1,10 @@
+var i = 1;
+while (i < 12) {
+    console.log(i);
+    i++;
+} //while loop ma bs condition deni hoti
+var v = 1;
+do {
+    console.log(i);
+    i++;
+} while (i < 12);
---
+++ wloop.ts
@@ -0,0 +1,10 @@
+// let i:number=1;
+// while(i<12){
+//     console.log(i);
+//     i++
+// } //while loop ma bs condition deni hoti
+let v:number=1;
+do{
+    console.log(v);
+    v++  
+}while(v<12)
