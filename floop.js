29d6e3c9fa32f02b17cf7d2fe27a84cd33e71557
rajayaseen <yaseenraja340@gmail.com>
2023-08-10 01:33:36 -0700

    for loop with examplees

---
+++ floop.js
@@ -0,0 +1,33 @@
+// let x=5
+// for (;x<10;){
+//     console.log(x)
+// }
+// for (x=1;x<5;x++){
+//     console.log(x);
+// }
+// x=10
+// for (;x>0;x--){
+//     console.log(x);
+// }
+// let countries= ["pakistan","afghanistan","iran","China","Pindi"];
+// let x=0
+// for (;x<countries.length;x++){
+//     console.log(countries[x]);
+//     for (let y=0;y<countries[x].length;y++){
+//         if (countries[x].charAt(y)=="a" || countries[x].charAt(y)=="A")
+//         console.log("+");
+//         else 
+//           console.log(countries[x].charAt(y))
+//  } //chartat ka matlb ha aleda aleda kary ga like pakistan k sath us k aleda aleda spel
+// }
+//task what is 2 dimentional array
+var num = Math.random() * 10;
+for (var z = 0; z < 5; z++) {
+    // num = Math.random()*10;
+    console.log(num);
+    //     if(num>5){
+    //         console.log("Number is more than 5" + num);     
+    //     } else {
+    // console.log("Number is less than 5" + num);
+}
+//}
